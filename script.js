8075e8ec7744c17e89db7452489da2d25ea67966
simidawalang <simidawalang@users.noreply.github.com>
2021-03-23 08:25:14 +0100

    Browser compatibility
    Tried fixing the backface-visibility property to work on Safari and iOS.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var login = document.querySelector(".login-card .login");
 var signup = document.querySelector(".login-card .sign-up");
-var loginForm = document.querySelector(".login-card .login form");
-var signupForm = document.querySelector(".login-card .sign-up form");
 
 function rotateToSignUp() {
-    login.classList.add("rotate-login");
     signup.classList.add("rotate-signup");
+    login.classList.add("rotate-login");   
 }
 
 
