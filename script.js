850f1cf1daeae0a05c36e50a566ca68bf5ba1894
simidawalang <simidawalang@users.noreply.github.com>
2021-03-23 11:13:11 +0100

    Fade In and Out

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 var login = document.querySelector(".login-card .login");
 var signup = document.querySelector(".login-card .sign-up");
+var loginForm = document.querySelector(".login-card .login form");
+var signupForm = document.querySelector(".login-card .sign-up form");
 
 function rotateToSignUp() {
     signup.classList.add("rotate-signup");
-    login.classList.add("rotate-login");   
+    signupForm.classList.remove("vanish");
+    login.classList.add("rotate-login");
+    loginForm.classList.add("vanish");
 }
 
 
 function rotateToLogin() {
     login.classList.remove("rotate-login");
+    signupForm.classList.add("vanish");
     signup.classList.remove("rotate-signup");
-    
+    loginForm.classList.remove("vanish");
 }
